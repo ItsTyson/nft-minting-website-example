76334bd7f8bca684e1ecb47b91f63fb5cb987ae1
ItsTyson <tysonlevyhowell@gmail.com>
2022-02-01 00:41:19 -0500

    update1

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -3,7 +3,7 @@ import React from "react";
 export default function Home() {
 	return (
 		<div className="page home">
-			<h1>Gently Used NFTs Looking For A new Home</h1>
+			<h1>Crpto girls from all across the metavers.</h1>
 			<p>Click the "Connect Wallet" Button above to access the minting page.</p>
 			<img src="hero.png" alt="Hero" width="100%" />
 		</div>
